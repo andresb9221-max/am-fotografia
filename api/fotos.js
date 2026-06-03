291c6360e3e21efd71d6fa50a968eeec52757ea6
Andres Barrera <andresb.9221@gmail.com>
2026-06-03 14:07:19 -0600

    Agregar API de fotos

---
+++ api/fotos.js
@@ -0,0 +1,52 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+connectionString: process.env.DATABASE_URL,
+ssl: {
+rejectUnauthorized: false
+}
+});
+
+module.exports = async (req, res) => {
+
+
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "GET");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+if (req.method !== "GET") {
+    return res.status(405).json({
+        error: "Método no permitido"
+    });
+}
+
+try {
+
+    const resultado = await pool.query(
+        `
+        SELECT
+            id,
+            nombre_archivo,
+            url_thumbnail
+        FROM fotos
+        ORDER BY id
+        LIMIT 20
+        `
+    );
+
+    return res.status(200).json(
+        resultado.rows
+    );
+
+} catch (error) {
+
+    console.log(error);
+
+    return res.status(500).json({
+        error: "Error servidor"
+    });
+}
+
+
+};
+
